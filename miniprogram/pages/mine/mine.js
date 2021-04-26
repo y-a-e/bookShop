dba03b349072c1c305bd3f4b79166da7630b3ade
lly <lly@email.com>
2021-04-26 15:59:58 +0800

    授权登录

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -20,7 +20,7 @@ Page({
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
     // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
-      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写 
       success: (res) => {
         console.log(res);
         this.setData({
@@ -31,7 +31,6 @@ Page({
     })
   },
   login: function (event) {
-    console.log(event);
     wx.navigateTo({
       url: 'login/login',
     })
